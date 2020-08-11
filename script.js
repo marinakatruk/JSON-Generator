827d74c88944c219e54f75dcf8572e3c56656275
Marina Katruk <m.katruk@outlook.com>
2020-08-11 11:01:44 +0300

    add inner object

--- script.js
+++ script.js
@@ -5,49 +5,28 @@ const resultObject = document.getElementById('object');
 const form = document.forms.form;
 const img = document.getElementById('img');
 const downloadButton = document.getElementById('download');
+const valueButton = document.getElementById('value-button');
+const innerObject = document.getElementById('inner-object');
+
 
 
 //показать/скрыть элемент(форма и картинка)
-const hiddenClassToggle = (toRemove, toAdd) => {
-    if (toRemove.classList.contains('hidden')) {
-        toAdd.classList.add('hidden');
-        toRemove.classList.remove('hidden');
+const hiddenClassToggle = (RemoveClass, AddClass) => {
+    if (RemoveClass.classList.contains('hidden')) {
+        AddClass.classList.add('hidden');
+        RemoveClass.classList.remove('hidden');
     }
 };
 
 plusButton.onclick = () => hiddenClassToggle(form, img);
 
-//сообщения-подсказки по заполнению полей
-const showHelp = (note) => {
-    note.style.display = 'block';
-}
-
-const hideHelp = (note) => {
-    note.style.display = 'none';
-}
-
-const helpKey = document.getElementById('help-key');
-
-form.key.onfocus = () => {
-    setTimeout (() => showHelp(helpKey), 100);
-    form.key.onblur = () => {
-        setTimeout (() => hideHelp(helpKey), 100);
-    }
-};
-
-const helpValue = document.getElementById('help-value');
-
-form.value.onfocus = () => {
-    setTimeout (() => showHelp(helpValue), 100);
-    form.value.onblur = () => {
-        setTimeout (() => hideHelp(helpValue), 100);
-    }
-};
-
 
 const addButton = form.elements.addbutton;
 const fields = form.querySelectorAll('.form__input');
 const blocks = form.querySelectorAll('.form__block');
+const innerFields = form.querySelectorAll('.inner__input');
+const innerBlocks = form.querySelectorAll('.inner__block');
+
 
 //сообщение об ошибке при заполнении
 const generateError = (text) => {
@@ -65,35 +44,78 @@ const removeValidation = () => {
     }
 };
 
-//валидация - нет пустых полей
-const checkFieldsPresense = () => {
+//валидация - все поля заполнены, максимальное кол-во символов
+const checkFieldsPresAndMax = () => {
+
     let valid = true;
-    for (let i = 0; i < fields.length; i += 1) {
-        if (!fields[i].value) {
+    const lettersAndNumbers = /^[0-9a-zA-Z]+$/;
+
+    if (innerObject.classList.contains('hidden')) {
+        for (let i = 0; i < fields.length; i += 1) {
+            let length = fields[i].value.length;
+            if (!fields[i].value) {
+                let error = generateError('Cannot be blank');
+                blocks[i].append(error);
+                valid = false;
+            } else if (length > 12) {
+                let error = generateError('Cannot be more than 12 symbols');
+                blocks[i].append(error);
+                valid = false;
+            } else if (!fields[i].value.match(lettersAndNumbers)) {
+                let error = generateError('Must contain only letters or numbers');
+                blocks[i].append(error);
+                valid = false;
+            }
+        }
+    } else {
+        let length = fields[0].value.length;
+        if (!fields[0].value) {
             let error = generateError('Cannot be blank');
-            blocks[i].append(error);
+            blocks[0].append(error);
+            valid = false;
+        } else if (length > 12) {
+            let error = generateError('Cannot be more than 12 symbols');
+            blocks[0].append(error);
             valid = false;
         }
+        for (let i = 0; i < innerFields.length; i += 1) {
+            let innerLength = innerFields[i].value.length;
+            if (!innerFields[i].value) {
+                let error = generateError('Cannot be blank');
+                innerBlocks[i].append(error);
+                valid = false;
+            } else if (innerLength > 12) {
+                let error = generateError('Cannot be more than 12 symbols');
+                innerBlocks[i].append(error);
+                valid = false;
+            } else if (!innerFields[i].value.match(lettersAndNumbers)) {
+                let error = generateError('Must contain only letters or numbers');
+                innerBlocks[i].append(error);
+                valid = false;
+            }
+        }
     }
+
     return valid;
 };
 
-//валидация - символы
-const checkFieldsValuesMatch = () => {
-    let valid = true;
-    const lettersAndNumbers = /^[0-9a-zA-Z]+$/;
-    const letAndNumAndProp = /^[0-9a-zA-Z]+|[0-9a-zA-Z]+[=][0-9a-zA-Z]+$/;
-    if (!form.key.value.match(lettersAndNumbers) && form.key.value) {
-        let error = generateError('Must contain only letters or numbers');
-        blocks[0].append(error);
-        valid = false;
-    } else if (!form.value.value.match(letAndNumAndProp) && form.value.value) {
-        let error = generateError('Must contain only letters or numbers or a property');
-        blocks[1].append(error);
-        valid = false;
-    }
-    return valid;
-};
+// //валидация - символы
+// const checkFieldsValuesMatch = () => {
+//     let valid = true;
+//     const lettersAndNumbers = /^[0-9a-zA-Z]+$/;
+//     const letAndNumAndProp = /^[0-9a-zA-Z]+|[0-9a-zA-Z]+[=][0-9a-zA-Z]+$/;
+//     if (!form.key.value.match(lettersAndNumbers) && form.key.value) {
+//         let error = generateError('Must contain only letters or numbers');
+//         blocks[0].append(error);
+//         valid = false;
+//     } 
+//     if (!form.value.value.match(letAndNumAndProp) && form.value.value) {
+//         let error = generateError('Must contain only letters or numbers or a property');
+//         blocks[1].append(error);
+//         valid = false;
+//     }
+//     return valid;
+// };
 
 //добавить свойство в итоговое поле
 const addPropertyString = () => {
@@ -122,6 +144,7 @@ const checkKeyNotDoubled = (key) => {
     return valid;
 };
 
+
 //добавить свойство в объект
 const AddPropertyToObject = () => {
     const newKey = form.key.value;
@@ -138,8 +161,8 @@ form.addEventListener('submit', function (event) {
     
     removeValidation();
 
-    if (checkFieldsPresense() && checkFieldsValuesMatch() && checkKeyNotDoubled(form.key.value)) {
-
+    if (checkFieldsPresAndMax() && checkFieldsValuesMatch() && checkKeyNotDoubled(form.key.value)) {
+       
         AddPropertyToObject();
 
         addPropertyString();
@@ -150,9 +173,8 @@ form.addEventListener('submit', function (event) {
 
         if (downloadButton.classList.contains('hidden')) {
             downloadButton.classList.remove('hidden');
-        } 
+        }
     }
-    console.log(newObject);
 });
 
 //убрать кнопку download
@@ -178,6 +200,15 @@ result.onclick = (event) => {
     hideDownloadButton();
     console.log(newObject);
 };
+
+
+const formBlockToHide = blocks[1];
+
+valueButton.onclick = () => {
+    hiddenClassToggle(innerObject, formBlockToHide);
+    console.log(innerObject);
+};
+
 
 
 
