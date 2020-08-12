77f0b723fa3eb5d93c14a112a1fd098042bca5f5
Marina Katruk <m.katruk@outlook.com>
2020-08-12 16:25:34 +0300

    add inner object to result

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const downloadButton = document.getElementById('download');
 const valueButton = document.getElementById('value-button');
 const innerObject = document.getElementById('inner-object');
 
+const addButton = form.elements.addbutton;
+const fields = form.querySelectorAll('.form__input');
+const blocks = form.querySelectorAll('.form__block');
+const innerFields = form.querySelectorAll('.inner__input');
+const innerBlocks = form.querySelectorAll('.inner__block');
 
 
 //показать/скрыть элемент(форма и картинка)
@@ -20,12 +25,11 @@ const hiddenClassToggle = (RemoveClass, AddClass) => {
 
 plusButton.onclick = () => hiddenClassToggle(form, img);
 
+const formBlockToHide = blocks[1];
 
-const addButton = form.elements.addbutton;
-const fields = form.querySelectorAll('.form__input');
-const blocks = form.querySelectorAll('.form__block');
-const innerFields = form.querySelectorAll('.inner__input');
-const innerBlocks = form.querySelectorAll('.inner__block');
+valueButton.onclick = () => {
+    hiddenClassToggle(innerObject, formBlockToHide);
+};
 
 
 //сообщение об ошибке при заполнении
@@ -44,12 +48,31 @@ const removeValidation = () => {
     }
 };
 
+// const checkAllFields = (fields, blocks, n) => {
+//     const lettersAndNumbers = /^[0-9a-zA-Z]+$/;
+//     let length = fields[n].value.length;
+//     let valid;
+//     if (!fields[n].value) {
+//         let error = generateError('Cannot be blank');
+//         blocks[n].append(error);
+//         valid = false;
+//     } else if (length > 12) {
+//         let error = generateError('Cannot be more than 12 symbols');
+//         blocks[n].append(error);
+//         valid = false;
+//     } else if (!fields[n].value.match(lettersAndNumbers)) {
+//         let error = generateError('Must contain only letters or numbers');
+//         blocks[n].append(error);
+//         valid = false;
+//     }
+//     return valid;
+// };
+
 //валидация - все поля заполнены, максимальное кол-во символов
-const checkFieldsPresAndMax = () => {
+const checkFieldsValidation = () => {
 
     let valid = true;
     const lettersAndNumbers = /^[0-9a-zA-Z]+$/;
-
     if (innerObject.classList.contains('hidden')) {
         for (let i = 0; i < fields.length; i += 1) {
             let length = fields[i].value.length;
@@ -67,6 +90,7 @@ const checkFieldsPresAndMax = () => {
                 valid = false;
             }
         }
+
     } else {
         let length = fields[0].value.length;
         if (!fields[0].value) {
@@ -75,6 +99,10 @@ const checkFieldsPresAndMax = () => {
             valid = false;
         } else if (length > 12) {
             let error = generateError('Cannot be more than 12 symbols');
+            blocks[0].append(error);
+            valid = false;
+        } else if (!fields[0].value.match(lettersAndNumbers)) {
+            let error = generateError('Must contain only letters or numbers');
             blocks[0].append(error);
             valid = false;
         }
@@ -95,39 +123,65 @@ const checkFieldsPresAndMax = () => {
             }
         }
     }
-
     return valid;
 };
 
-// //валидация - символы
-// const checkFieldsValuesMatch = () => {
-//     let valid = true;
-//     const lettersAndNumbers = /^[0-9a-zA-Z]+$/;
-//     const letAndNumAndProp = /^[0-9a-zA-Z]+|[0-9a-zA-Z]+[=][0-9a-zA-Z]+$/;
-//     if (!form.key.value.match(lettersAndNumbers) && form.key.value) {
-//         let error = generateError('Must contain only letters or numbers');
-//         blocks[0].append(error);
-//         valid = false;
-//     } 
-//     if (!form.value.value.match(letAndNumAndProp) && form.value.value) {
-//         let error = generateError('Must contain only letters or numbers or a property');
-//         blocks[1].append(error);
-//         valid = false;
-//     }
-//     return valid;
-// };
+const innerKey = document.getElementById('inner-key');
+const innerValue = document.getElementById('inner-value');
+
+const generateProperty = (key, value) => {
+    let elem = document.createElement('div');
+    elem.className = 'property';
+    elem.setAttribute('data-key', key.value);
+    elem.innerHTML = `<span>"${key.value}"</span> : <span>"${value.value}"</span>,`;
+    return elem;
+};
+
+const createMinusButton = () => {
+    let button = document.createElement('div');
+    button.className = 'result__button minus';
+    button.innerHTML = '-';
+    return button;
+};
 
 //добавить свойство в итоговое поле
 const addPropertyString = () => {
-    const newProp = document.createElement('div');
-    newProp.className = 'property';
-    newProp.setAttribute('data-key', form.key.value);
-    newProp.innerHTML = `<span>"${form.key.value}"</span> : <span>"${form.value.value}"</span>,`;
-    plusButton.before(newProp);
-    const minusButton = document.createElement('div');
-    minusButton.className = 'result__button minus';
-    minusButton.innerHTML = '-';
-    newProp.append(minusButton);
+    if (innerObject.classList.contains('hidden')) {
+        const newProp = generateProperty(form.key, form.value);
+        plusButton.before(newProp);
+        const minusButton = createMinusButton();
+        newProp.append(minusButton);
+    
+    } else {
+        const newKey = document.createElement('div');
+        newKey.className = 'property';
+        newKey.setAttribute('data-key', form.key.value);
+        newKey.innerHTML = `"<span>${form.key.value}" : {</span>`;
+        plusButton.before(newKey);
+        
+        const minusButton = createMinusButton();
+        newKey.append(minusButton);
+        
+        const innerProp = generateProperty(innerKey, innerValue);
+        newKey.append(innerProp);
+        innerProp.style.marginLeft = newKey.firstElementChild.offsetWidth + 'px';
+        
+        const innerMinusButton = createMinusButton();
+        innerProp.append(innerMinusButton);
+
+        const innerPlusButton = document.createElement('div');
+        innerPlusButton.className = 'result__button';
+        innerPlusButton.style.marginLeft = newKey.firstElementChild.offsetWidth + 'px';
+        innerPlusButton.setAttribute('id', 'inner-plus');
+        innerPlusButton.innerHTML = '+';
+        newKey.append(innerPlusButton);
+        
+        const closingBrace = document.createElement('div');
+        closingBrace.innerHTML = '}';
+        newKey.append(closingBrace);
+        closingBrace.style.marginLeft = newKey.firstElementChild.offsetWidth + 'px';
+
+    }
 };
 
 const newObject = {};
@@ -149,10 +203,25 @@ const checkKeyNotDoubled = (key) => {
 const AddPropertyToObject = () => {
     const newKey = form.key.value;
     const newValue = form.value.value;
-    if (typeof newKey === 'number') {
-        newObject[String(newKey)] = newValue;
+    if (innerObject.classList.contains('hidden')) {
+        if (typeof newKey === 'number') {
+            newObject[String(newKey)] = newValue;
+        }
+        newObject[newKey] = newValue;
     }
-    newObject[newKey] = newValue;
+    else {
+        const propObject = {};
+        if (typeof innerKey.value === 'number') {
+            propObject[String(innerKey.value)] = innerValue.value;
+        }
+        propObject[innerKey.value] = innerValue.value;
+
+        if (typeof newKey === 'number') {
+            newObject[String(newKey)] = propObject;
+        }
+        newObject[newKey] = propObject;
+        console.log(newObject);
+    }
 };
 
 //обработчик на отправку формы
@@ -161,15 +230,17 @@ form.addEventListener('submit', function (event) {
     
     removeValidation();
 
-    if (checkFieldsPresAndMax() && checkFieldsValuesMatch() && checkKeyNotDoubled(form.key.value)) {
+    if (checkFieldsValidation() && checkKeyNotDoubled(form.key.value)) {
        
         AddPropertyToObject();
 
         addPropertyString();
 
         fields.forEach(field => field.value = '');
+        innerFields.forEach(field => field.value = '');
 
         hiddenClassToggle(img, form);
+        hiddenClassToggle(formBlockToHide, innerObject);
 
         if (downloadButton.classList.contains('hidden')) {
             downloadButton.classList.remove('hidden');
@@ -192,23 +263,23 @@ result.onclick = (event) => {
     let minus = event.target.closest('div.minus');
     if (!minus) return;
 
-    minus.parentNode.remove();
+    const minusParentOfParent = minus.parentNode.parentNode;
+    if (!minusParentOfParent.hasAttribute('data-key')) {
+        const keyToDelete = minus.parentNode.dataset.key;
+        delete newObject[keyToDelete];
+    } else {
+        const outerKeyToDelete = minusParentOfParent.dataset.key;
+        const innerKeyToDelete = minus.parentNode.dataset.key;
+        delete newObject[outerKeyToDelete][innerKeyToDelete];
+        console.log(newObject);
+    }
 
-    const keyToDelete = minus.parentNode.dataset.key;
-    delete newObject[keyToDelete];
+
+    minus.parentNode.remove();
 
     hideDownloadButton();
     console.log(newObject);
 };
-
-
-const formBlockToHide = blocks[1];
-
-valueButton.onclick = () => {
-    hiddenClassToggle(innerObject, formBlockToHide);
-    console.log(innerObject);
-};
-
 
 
 
